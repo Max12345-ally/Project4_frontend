c8b64a4720659a653bc0e535c03695810a912074
maxbond <ceo@rf.ru>
2023-01-05 21:54:35 +0300

    host from .end

--- project4_frontend/src/api.js
+++ project4_frontend/src/api.js
@@ -1,5 +1,6 @@
 export function myFetch(path, config = {}) {
-  const HOST = "http://localhost:4000";
+  const HOST = process.env.API;
+  //const HOST = "http://localhost:4000";
   // const HOST = "http://185.20.225.250";
   return fetch(`${HOST}/${path}`, {
     ...config,
