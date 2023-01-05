53b433cebed7ef82958b436b4c9bf0282594e3db
maxbond <ceo@rf.ru>
2023-01-05 22:18:57 +0300

    move to root

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export function myFetch(path, config = {}) {
-  const HOST = process.env.API;
+  const HOST = process.env.REACT_APP_API;
   //const HOST = "http://localhost:4000";
   // const HOST = "http://185.20.225.250";
   return fetch(`${HOST}/${path}`, {
